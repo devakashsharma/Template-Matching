77a180f363e45c268215fd1ebbc5cd242cc6cc31
Akash Sharma <127339771+SharmaAkash001@users.noreply.github.com>
2023-09-19 01:14:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
 for method in methods:
     img2 = img.copy()
 
-    result = cv2.matchTemplate(img2, template, method) # Check Testing.py for better explanation
+    result = cv2.matchTemplate(img2, template, method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
         location = min_loc
